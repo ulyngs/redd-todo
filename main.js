f01f920762a5d74382753b67530fd228fcccff7a
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2025-12-01 09:36:54 +0100

    fill in the help menu for mac

--- main.js
+++ main.js
@@ -1,6 +1,135 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, shell } = require('electron');
 const path = require('path');
+
 let mainWindow;
+
+function createMenu() {
+  const isMac = process.platform === 'darwin';
+
+  const template = [
+    // { role: 'appMenu' }
+    ...(isMac ? [{
+      label: app.name,
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideOthers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []),
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        isMac ? { role: 'close' } : { role: 'quit' }
+      ]
+    },
+    // { role: 'editMenu' }
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac ? [
+          { role: 'pasteAndMatchStyle' },
+          { role: 'delete' },
+          { role: 'selectAll' },
+          { type: 'separator' },
+          {
+            label: 'Speech',
+            submenu: [
+              { role: 'startSpeaking' },
+              { role: 'stopSpeaking' }
+            ]
+          }
+        ] : [
+          { role: 'delete' },
+          { type: 'separator' },
+          { role: 'selectAll' }
+        ])
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac ? [
+          { type: 'separator' },
+          { role: 'front' },
+          { type: 'separator' },
+          { role: 'window' }
+        ] : [
+          { role: 'close' }
+        ])
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Report an Issue',
+          click: async () => {
+            await shell.openExternal('https://github.com/ulyngs/redd-todo/issues')
+          }
+        },
+        {
+          label: 'Contact Us',
+          click: async () => {
+             await shell.openExternal('mailto:team@reddfocus.org')
+          }
+        },
+        {
+          label: 'Privacy Policy',
+          click: async () => {
+             await shell.openExternal('https://ulyngs.github.io/redd-todo/privacy_policy')
+          }
+        },
+        {
+          label: 'Our Research',
+          click: async () => {
+            await shell.openExternal('https://reddfocus.org/research')
+          }
+        },
+        {
+          label: 'Who We Are',
+          click: async () => {
+            await shell.openExternal('https://www.reddfocus.org/#team-anchor')
+          }
+        }
+      ]
+    }
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 400,
@@ -97,7 +226,10 @@ ipcMain.on('exit-focus-mode', () => {
     }
   }
 });
-app.whenReady().then(createMainWindow);
+app.whenReady().then(() => {
+  createMainWindow();
+  createMenu();
+});
 app.on('window-all-closed', () => {
   app.quit();
 });
